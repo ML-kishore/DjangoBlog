d8a8a51213362756d9d72e5f6e9e455186a2a5ef
kishore <kishoreml94@gmail.com>
2025-08-21 17:56:06 +0530

    Fix Profile save method with *args and **kwargs

--- users/models.py
+++ users/models.py
@@ -3,19 +3,23 @@ from django.contrib.auth.models import User
 from PIL import Image
 
 # Create your models here.
+from PIL import Image
+from django.db import models
+from django.contrib.auth.models import User
+
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE) # one user will have one profile picture (one2onefield)
-    image = models.ImageField(default='default.jpg',upload_to='profile_pics')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # one user will have one profile picture
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return f"{self.user.username} Profile"
     
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):   # ✅ accept args & kwargs
+        super().save(*args, **kwargs)  # ✅ pass them to parent save()
 
         img = Image.open(self.image.path)
 
         if img.height > 300 or img.width > 300:
-            output_size = (300,300)
+            output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
